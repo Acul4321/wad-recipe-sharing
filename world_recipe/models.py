2952e4be3deb6ddcbf9780db0532f1465954bfd7
Luca Rogers <2886751r@student.gla.ac.uk>
2025-03-11 15:13:04 +0000

    base project url setup

--- world_recipe/models.py
+++ world_recipe/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class Recipe(models.Model):
     #author/user foreign key
     name = models.CharField(max_length=200)
--- world_recipe/views.py
+++ world_recipe/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    response = render(request, 'world_recipe/index.html')
-    return response
+    return HttpResponse("Hello, world. You're at the world_recipe index.")
+    # response = render(request, 'world_recipe/index.html')
+    # return response
