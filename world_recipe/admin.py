6d97c7b512ed2c8dc3b3fa77f158c28d4aac7fde
Luca Rogers <2886751r@student.gla.ac.uk>
2025-03-20 16:45:53 +0000

    admin display panel

--- world_recipe/admin.py
+++ world_recipe/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
-from world_recipe.models import UserProfile#, Recipe, Comment, Rating, RecipeImages
+from world_recipe.models import UserProfile, Recipe, Comment, Rating, RecipeImages
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'originID', 'profile_picture')
 
-admin.site.register(UserProfile)
-# admin.site.register(Recipe)
-# admin.site.register(Comment)
-# admin.site.register(Rating)
-# admin.site.register(RecipeImages)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'authorID', 'originID', 'meal_type')
+    list_filter = ('meal_type', 'originID')
+    search_fields = ['title', 'ingredients']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('recipeID', 'userID', 'timestamp', 'content')
+    list_filter = ('timestamp',)
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('recipeID', 'userID', 'rating')
+    list_filter = ('rating',)
+
+class RecipeImagesAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'image')
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Rating, RatingAdmin)
+admin.site.register(RecipeImages, RecipeImagesAdmin)
