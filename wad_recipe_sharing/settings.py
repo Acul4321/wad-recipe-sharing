b80fce6d702cf866994acd7c738c6430e3a763b7
Luca Rogers <lucarogers666@gmail.com>
2025-03-21 20:28:51 +0000

    map and env file

--- wad_recipe_sharing/settings.py
+++ wad_recipe_sharing/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +26,9 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u^5s_q@ygprwo3sk(g5v0=f#5*=v!*@i_d33sp+41q6r$nno(&'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+# Google Maps API Key
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -133,3 +139,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
