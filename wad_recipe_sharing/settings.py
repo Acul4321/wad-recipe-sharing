645e3b1e290d2ed85d1a78645ef7169621443878
Luca Rogers <lucarogers666@gmail.com>
2025-03-26 15:29:21 +0000

    pythonanywhere deployment fixes

--- wad_recipe_sharing/settings.py
+++ wad_recipe_sharing/settings.py
@@ -28,7 +28,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-default-key-for-development')
 # Google Maps API Key
-GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
+GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,4 +139,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# PythonAnywhere media serving
+if not DEBUG:
+    MEDIA_ROOT = '/home/acul4321/acul4321.pythonanywhere.com/media'
+    STATIC_ROOT = '/home/acul4321/acul4321.pythonanywhere.com/static'
 
